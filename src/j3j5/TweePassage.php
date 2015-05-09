148aa67388ebf5f920b81a58d8b61a2974bf654e
Julio Foulquié <julio@tnwlabs.com>
2015-05-09 21:02:48 +0200

    First version of the library.

---
+++ src/j3j5/TweePassage.php
@@ -0,0 +1,77 @@
+<?php
+
+/**
+ * TweePassage
+ *
+ * An object to represent the Twee entity (passage).
+ *
+ * @author Julio Foulquié
+ * @version 0.1.0
+ *
+ * 09 May 2015
+ */
+
+namespace j3j5;
+
+class TweePassage {
+
+	private $log;
+
+	public $title;
+	public $text;
+	public $links;
+	public $modifier;
+	public $tags;
+	public $created;
+	public $modified;
+
+	/**
+	 * Valid link patterns for Title="Open the cage door" and Action="Devoured by Lions" are:
+	 * 	-) [[Open the cage door->Devoured by Lions]]
+	 * 	-) [[Devoured by Lions<-Open the cage door]]
+	 * 	-) [[Devoured by Lions|Open the cage door]]
+	 */
+	private $links_pattern = "/\[\[(.*?)([<\->]{2}|\|)(.*?)\]\]/i";
+
+	public function __construct(array $passage, $log) {
+		if(!isset($passage['title'], $passage['text'], $passage['tags'])) {
+			throw new \Exception("Wrong entity");
+		}
+
+		$this->log = $log;
+		$this->parse_passage($passage);
+	}
+
+	private function parse_passage($passage) {
+		$this->title	= $passage['title'];
+		$this->text		= $passage['text'];
+		$this->modifier	= $passage['modifier'];
+		$this->tags		= $passage['tags'];
+		$this->created	= $passage['created'];
+		$this->modified	= $passage['modified'];
+		$this->parse_links($passage['text']);
+	}
+
+	private function parse_links($text) {
+		$offset = 0;
+		$matches = array();
+		// Extract all links from the text of the passage (if they exist)
+		while(preg_match($this->links_pattern, $text, $matches, 0, $offset)) {
+			switch($matches[2]) {
+				case '->':
+					$this->links[] = array('text' => $matches[1], 'link' => $matches[3]);
+					break;
+				case '<-':
+				case '|':
+					$this->links[] = array('text' => $matches[3], 'link' => $matches[1]);
+					break;
+				default:
+					$this->log->addWarning("Something that looked like a link could not be parsed.");
+					$this->log->addWarning($text);
+					break;
+			}
+			$offset = mb_stripos($text, $matches[0]) + mb_strlen($matches[0]);
+		}
+	}
+
+}
---
+++ src/j3j5/TweeStory.php
@@ -0,0 +1,193 @@
+<?php
+
+/**
+ * TweeParser
+ *
+ * A parser for JSON Twee files to create a meaningful object oriented approach.
+ *
+ * @author Julio Foulquie
+ * @version 0.1.0
+ *
+ * 09 May 2015
+ */
+
+namespace j3j5;
+
+use \Monolog\Logger;
+use \Monolog\Handler\StreamHandler;
+
+class TweeStory {
+
+	private $log;
+
+	private $title;
+	private $passages;
+
+	private $scripts;
+	private $stylesheets;
+
+	private $current_passage;
+
+	public function __construct($file_path) {
+
+		// Set up the logging
+		$min_log_level = Logger::DEBUG;
+		$this->log = new Logger('twee-parser');
+		if(PHP_SAPI == 'cli') {
+			$this->log->pushHandler(new StreamHandler("php://stdout", $min_log_level));
+		} else {
+// 			$this->log->pushHandler(new StreamHandler(dirname(__DIR__) . '/data/logs/last-stream.log', $min_log_level));
+		}
+
+		// Read and process the given file
+		if(is_file($file_path)) {
+			$json = file_get_contents($file_path);
+			if(!empty($json) && is_string($json)) {
+				$result = $this->process_json($json);
+				if(empty($result)) {
+					$this->log->addWarning("Something went wrong while processing the json.");
+					throw new \Exception("Error processing");
+				}
+			} else {
+				$this->log->addWarning("Something went wrong while reading the json.");
+				throw new \Exception("Error reading");
+			}
+		} else {
+			$this->log->addError("$file_path is NOT a file.");
+			throw new \Exception("Arg is not a file");
+		}
+	}
+
+	/**
+	 * Get the current position on the story
+	 *
+	 * @param void
+	 *
+	 * @return TweePassage $current_passage The current passage on the story.
+	 *
+	 * @author Julio Foulquie <jfoulquie@gmail.com>
+	 */
+	public function get_current_passage() {
+		return $this->passages[$this->current_passage];
+	}
+
+	/**
+	 * Follow a given link to move on the story.
+	 *
+	 * @param String $next_passage The name of the next passage the story should move on
+	 *
+	 * @return TweePassage|Bool The new passage on the story or FALSE if it does not exist.
+	 *
+	 * @author Julio Foulquie <jfoulquie@gmail.com>
+	 */
+	public function follow_link($next_passage) {
+		if(isset($this->passages[$next_passage])) {
+			$this->current_passage = $next_passage;
+			return $this->get_current_passage();
+		}
+		$this->log->addWarning("Trying to follow a dead link.");
+	}
+
+
+	/**
+	 * Process the JSON exported from Twee and create the story object
+	 *
+	 * @param String $json The string containig the JSON object
+	 *
+	 * @return void
+	 *
+	 * @author Julio Foulquie <jfoulquie@gmail.com>
+	 */
+	private function process_json($json) {
+
+		$twee_array = json_decode($json, TRUE);
+
+		if(!isset($twee_array['data']) OR empty($twee_array['data'])) {
+			$this->log->addWarning("The provided JSON did not decode or is empty.");
+			$this->log->addWarning($json);
+			return FALSE;
+		}
+
+		foreach($twee_array['data'] AS $entity) {
+			$type = $this->get_type_from_tags($entity['tags']);
+			if(empty($type)){
+				$type = $this->get_type_from_title($entity['title']);
+			}
+			switch($type) {
+				case 'passage':
+					try {
+						$passage = new TweePassage($entity, $this->log);
+						$this->passages[$passage->title] = $passage;
+						if(empty($this->current_passage)) {
+							$this->current_passage = $passage->title;
+						}
+					} catch(Exception $e) {
+						$this->log->addWarning($e->getMessage());
+					}
+					break;
+				case 'title':
+					$this->title = $entity['text'];
+					break;
+				case 'script':
+					$this->scripts[] = $entity['text'];
+					break;
+				case 'stylesheet':
+					$this->stylesheets[] = $entity['text'];
+					break;
+				default:
+					$this->log->addWarning("Unknown entity type!");
+					$this->log->addWarning(print_r($entity, TRUE));
+					break;
+			}
+		}
+		return TRUE;
+	}
+
+	/**
+	 * Get the type of entity based on its tags.
+	 *
+	 * @param Array $tags The tags of the entity
+	 *
+	 * @return String|Bool The type of entity or FALSE
+	 *
+	 * @author Julio Foulquie <jfoulquie@gmail.com>
+	 */
+	private function get_type_from_tags($tags) {
+		$tag_types = array('stylesheet', 'script');
+
+		foreach($tag_types as $tag) {
+			if(in_array($tag, $tags)) {
+				return $tag;
+			}
+		}
+
+		return FALSE;
+	}
+
+	/**
+	 * Get the type of entity based on its title.
+	 *
+	 * @param String $title The title of the entity
+	 *
+	 * @return String The type of entity
+	 *
+	 * @author Julio Foulquie <jfoulquie@gmail.com>
+	 */
+	private function get_type_from_title($title) {
+		$title_types = array(
+			'stylesheet'	=> 'UserStylesheet',
+			'script'		=> 'UserScript',
+			'title'			=> 'StoryTitle',
+		);
+
+		foreach($title_types AS $type => $name) {
+			if($title == $name) {
+				return $type;
+			}
+		}
+		// Let's assume if the title isn't on the list, the entity is a passage
+		return 'passage';
+	}
+
+
+}
