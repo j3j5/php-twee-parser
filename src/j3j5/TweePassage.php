974c28179d907a3c99b86801af1578f04e30b64b
Julio Foulquie <jfoulquie@gmail.com>
2015-11-29 15:10:17 -0300

    Remove monolog dependency and follow PSR-2.

--- src/j3j5/TweePassage.php
+++ src/j3j5/TweePassage.php
@@ -13,75 +13,73 @@
 
 namespace j3j5;
 
-class TweePassage {
+class TweePassage
+{
+    private $raw_links;
 
-	private $log;
-	private $raw_links;
+    public $title;
+    public $text;
+    public $links;
+    public $modifier;
+    public $tags;
+    public $created;
+    public $modified;
 
-	public $title;
-	public $text;
-	public $links;
-	public $modifier;
-	public $tags;
-	public $created;
-	public $modified;
+    /**
+     * Valid link patterns for Title="Open the cage door" and Action="Devoured by Lions" are:
+     * 	-) [[Open the cage door->Devoured by Lions]]
+     * 	-) [[Devoured by Lions<-Open the cage door]]
+     * 	-) [[Devoured by Lions|Open the cage door]]
+     */
+    private $links_pattern = "/\[\[(.*?)([<\->]{2}|\|)(.*?)\]\]/i";
 
-	/**
-	 * Valid link patterns for Title="Open the cage door" and Action="Devoured by Lions" are:
-	 * 	-) [[Open the cage door->Devoured by Lions]]
-	 * 	-) [[Devoured by Lions<-Open the cage door]]
-	 * 	-) [[Devoured by Lions|Open the cage door]]
-	 */
-	private $links_pattern = "/\[\[(.*?)([<\->]{2}|\|)(.*?)\]\]/i";
+    public function __construct(array $passage)
+    {
+        if (!isset($passage['title'], $passage['text'], $passage['tags'])) {
+            throw new \Exception("Wrong entity");
+        }
 
-	public function __construct(array $passage, $log) {
-		if(!isset($passage['title'], $passage['text'], $passage['tags'])) {
-			throw new \Exception("Wrong entity");
-		}
+        $this->parse_passage($passage);
+    }
 
-		$this->log = $log;
-		$this->parse_passage($passage);
-	}
-
-	private function parse_passage($passage) {
-		$this->title		= $passage['title'];
-		$this->modifier		= $passage['modifier'];
-		$this->tags			= $passage['tags'];
-		$this->created		= $passage['created'];
-		$this->modified		= $passage['modified'];
-		$this->links		= array();
-		$this->raw_links	= array();
-		$this->parse_links($passage['text']);
+    private function parse_passage($passage)
+    {
+        $this->title        = $passage['title'];
+        $this->modifier        = $passage['modifier'];
+        $this->tags            = $passage['tags'];
+        $this->created        = $passage['created'];
+        $this->modified        = $passage['modified'];
+        $this->links        = array();
+        $this->raw_links    = array();
+        $this->parse_links($passage['text']);
 
         // Clean up links from the text
-		foreach($this->raw_links AS $link) {
-			$passage['text'] = str_replace($link, '', $passage['text']);
-		}
-		$this->text		= $passage['text'];
-	}
+        foreach ($this->raw_links as $link) {
+            $passage['text'] = str_replace($link, '', $passage['text']);
+        }
+        $this->text        = $passage['text'];
+    }
 
-	private function parse_links($text) {
-		$offset = 0;
-		$matches = array();
-		// Extract all links from the text of the passage (if they exist)
-		while(preg_match($this->links_pattern, $text, $matches, 0, $offset)) {
-			switch($matches[2]) {
-				case '->':
-					$this->links[] = array('text' => $matches[1], 'link' => $matches[3]);
-					$this->raw_links[] = $matches[0];
-					break;
-				case '<-':
-				case '|':
-					$this->links[] = array('text' => $matches[3], 'link' => $matches[1]);
-					$this->raw_links[] = $matches[0];
-					break;
-				default:
-					$this->log->addWarning("Something that looked like a link could not be parsed.");
-					$this->log->addWarning($text);
-					break;
-			}
-			$offset = mb_stripos($text, $matches[0]) + mb_strlen($matches[0]);
-		}
-	}
-
+    private function parse_links($text)
+    {
+        $offset = 0;
+        $matches = array();
+        // Extract all links from the text of the passage (if they exist)
+        while (preg_match($this->links_pattern, $text, $matches, 0, $offset)) {
+            switch ($matches[2]) {
+                case '->':
+                    $this->links[] = array('text' => $matches[1], 'link' => $matches[3]);
+                    $this->raw_links[] = $matches[0];
+                    break;
+                case '<-':
+                case '|':
+                    $this->links[] = array('text' => $matches[3], 'link' => $matches[1]);
+                    $this->raw_links[] = $matches[0];
+                    break;
+                default:
+                    throw new \RuntimeException("Something that looked like a link could not be parsed.");
+            }
+            $offset = mb_stripos($text, $matches[0]) + mb_strlen($matches[0]);
+        }
+    }
 }
--- src/j3j5/TweeStory.php
+++ src/j3j5/TweeStory.php
@@ -16,276 +16,251 @@
 
 namespace j3j5;
 
-use \Monolog\Logger;
-use \Monolog\Handler\StreamHandler;
+class TweeStory
+{
+    private $title;
+    private $passages;
 
-class TweeStory {
+    private $scripts;
+    private $stylesheets;
 
-	private $log;
+    private $current_passage;
 
-	private $title;
-	private $passages;
+    private $max_history_size;
+    private $history;
+    private $history_offset;
 
-	private $scripts;
-	private $stylesheets;
+    private $storyline;
 
-	private $current_passage;
+    public function __construct($file_path)
+    {
+        $this->history_offset = 0;
+        $this->max_history_size = 50;
 
-	private $max_history_size;
-	private $history;
-	private $history_offset;
+        // Read and process the given file
+        if (is_file($file_path)) {
+            $json = file_get_contents($file_path);
+            if (!empty($json) && is_string($json)) {
+                $result = $this->process_json($json);
+                if (empty($result)) {
+                    throw new \Exception("Error processing");
+                }
+            } else {
+                throw new \Exception("Error reading");
+            }
+        } else {
+            throw new \Exception("Arg is not a file");
+        }
+    }
 
-	private $storyline;
+    /**
+     *              ⎈⎈⎈⎈⎈⎈⎈⎈⎈⎈
+     *              NAVIGATION
+     *              ⎈⎈⎈⎈⎈⎈⎈⎈⎈⎈
+     */
 
-	public function __construct($file_path) {
+    /**
+     * Get the current position on the story
+     *
+     * @param void
+     *
+     * @return TweePassage $current_passage The current passage on the story.
+     *
+     * @author Julio Foulquie <jfoulquie@gmail.com>
+     */
+    public function get_current_passage()
+    {
+        return $this->passages[$this->current_passage];
+    }
 
-		$this->history_offset = 0;
-		$this->max_history_size = 50;
+    /**
+     * Undo the last action from the user. You can use this function
+     * to browse back the actions of the user.
+     *
+     * @param void
+     *
+     * @return TweePassage|Bool
+     *
+     * @author Julio Foulquie <jfoulquie@gmail.com>
+     */
+    public function undo()
+    {
+        end($this->history);
+        for ($i=0; $i<$this->history_offset; $i++) {
+            prev($this->history);
+        }
+        $index = prev($this->history);
+        if (!empty($index)) {
+            $this->current_passage = $index;
+            $this->history_offset++;
+            $this->storyline = array_slice($this->storyline, 0, -1);
+            return $this->get_current_passage();
+        }
+        return false;
+    }
 
-		// Set up the logging
-		$min_log_level = Logger::DEBUG;
-		$this->log = new Logger('twee-parser');
-		if(PHP_SAPI == 'cli') {
-			$this->log->pushHandler(new StreamHandler("php://stdout", $min_log_level));
-		}
-//        else {
-// 			$this->log->pushHandler(new StreamHandler(dirname(__DIR__) . '/data/logs/last-stream.log', $min_log_level));
-//		}
+    /**
+     * Redo the last undo from the user. You can use this function
+     * to browse forward all the undos of the user.
+     *
+     * @param void
+     *
+     * @return TweePassage|Bool
+     *
+     * @author Julio Foulquie <jfoulquie@gmail.com>
+     */
+    public function redo()
+    {
+        end($this->history);
+        for ($i=0; $i<$this->history_offset-1; $i++) {
+            prev($this->history);
+        }
+        $index = current($this->history);
+        if (!empty($index)) {
+            $this->current_passage = $index;
+            $this->history_offset--;
+            $this->storyline[] = $this->current_passage;
+        }
+        return $this->get_current_passage();
+    }
 
-		// Read and process the given file
-		if(is_file($file_path)) {
-			$json = file_get_contents($file_path);
-			if(!empty($json) && is_string($json)) {
-				$result = $this->process_json($json);
-				if(empty($result)) {
-					$this->log->addWarning("Something went wrong while processing the json.");
-					throw new \Exception("Error processing");
-				}
-			} else {
-				$this->log->addWarning("Something went wrong while reading the json.");
-				throw new \Exception("Error reading");
-			}
-		} else {
-			$this->log->addError("$file_path is NOT a file.");
-			throw new \Exception("Arg is not a file");
-		}
-	}
+    /**
+     * Follow a given link to move on the story.
+     *
+     * @param String $next_passage The name of the next passage the story should move on
+     *
+     * @return TweePassage|Bool The new passage on the story or FALSE if it does not exist.
+     *
+     * @author Julio Foulquie <jfoulquie@gmail.com>
+     */
+    public function follow_link($next_passage)
+    {
+        // Check whether that's a valid next passage
+        $current = $this->get_current_passage();
+        $valid_link = false;
+        if (is_array($current->links)) {
+            foreach ($current->links as $link) {
+                if ($link['link'] == $next_passage) {
+                    $valid_link = true;
+                }
+            }
+        }
 
-	/**
-	 * 				⎈⎈⎈⎈⎈⎈⎈⎈⎈⎈
-	 * 				    NAVIGATION
-	 * 				⎈⎈⎈⎈⎈⎈⎈⎈⎈⎈
-	 */
+        if ($valid_link && isset($this->passages[$next_passage])) {
+            $this->current_passage = $next_passage;
+            $this->history[] = $this->current_passage;
+            $this->storyline[] = $this->current_passage;
 
-	/**
-	 * Get the current position on the story
-	 *
-	 * @param void
-	 *
-	 * @return TweePassage $current_passage The current passage on the story.
-	 *
-	 * @author Julio Foulquie <jfoulquie@gmail.com>
-	 */
-	public function get_current_passage() {
-		$this->log->addDebug('Current passage--> ' . $this->current_passage);
-		return $this->passages[$this->current_passage];
-	}
+            // Reset the redo history
+            if ($this->history_offset != 0) {
+                $this->history = array_slice($this->history, 0, -$this->history_offset);
+                $this->history_offset = 0;
+            }
+            return $this->get_current_passage();
+        }
+    }
 
-	/**
-	 * Undo the last action from the user. You can use this function
-	 * to browse back the actions of the user.
-	 *
-	 * @param void
-	 *
-	 * @return TweePassage|Bool
-	 *
-	 * @author Julio Foulquie <jfoulquie@gmail.com>
-	 */
-	public function undo() {
-		end($this->history);
-		for($i=0; $i<$this->history_offset; $i++){
-			prev($this->history);
-		}
-		$index = prev($this->history);
-		if(!empty($index)) {
-			$this->current_passage = $index;
-			$this->history_offset++;
-			$this->storyline = array_slice($this->storyline, 0, -1);
-			return $this->get_current_passage();
-		}
-		return FALSE;
-	}
+    /**
+     *              ☣☣☣☣☣☣☣☣☣☣☣☣
+     *              Internal functions
+     *              ☣☣☣☣☣☣☣☣☣☣☣☣
+     */
 
-	/**
-	 * Redo the last undo from the user. You can use this function
-	 * to browse forward all the undos of the user.
-	 *
-	 * @param void
-	 *
-	 * @return TweePassage|Bool
-	 *
-	 * @author Julio Foulquie <jfoulquie@gmail.com>
-	 */
-	public function redo() {
-		if($this->history_offset == 0) {
-			$this->log->addInfo("There are no actions to redo.");
-		}
-		end($this->history);
-		for($i=0; $i<$this->history_offset-1; $i++){
-			prev($this->history);
-		}
-		$index = current($this->history);
-		if(!empty($index)) {
-			$this->current_passage = $index;
-			$this->history_offset--;
-			$this->storyline[] = $this->current_passage;
-		}
-		return $this->get_current_passage();
-	}
+    /**
+     * Process the JSON exported from Twee and create the story object
+     *
+     * @param String $json The string containig the JSON object
+     *
+     * @return void
+     *
+     * @author Julio Foulquie <jfoulquie@gmail.com>
+     */
+    private function process_json($json)
+    {
+        $twee_array = json_decode($json, true);
 
-	/**
-	 * Follow a given link to move on the story.
-	 *
-	 * @param String $next_passage The name of the next passage the story should move on
-	 *
-	 * @return TweePassage|Bool The new passage on the story or FALSE if it does not exist.
-	 *
-	 * @author Julio Foulquie <jfoulquie@gmail.com>
-	 */
-	public function follow_link($next_passage) {
-		// Check whether that's a valid next passage
-		$current = $this->get_current_passage();
-		$valid_link = FALSE;
-		if(is_array($current->links)) {
-			foreach($current->links AS $link){
-				if($link['link'] == $next_passage) {
-					$valid_link = TRUE;
-				}
-			}
-		}
+        if (!isset($twee_array['data']) or empty($twee_array['data'])) {
+            throw new RuntimeException("The provided JSON did not decode or is empty.");
+        }
 
-		if($valid_link && isset($this->passages[$next_passage])) {
-			$this->current_passage = $next_passage;
-			$this->history[] = $this->current_passage;
-			$this->storyline[] = $this->current_passage;
+        foreach ($twee_array['data'] as $entity) {
+            $type = $this->get_type_from_tags($entity['tags']);
+            if (empty($type)) {
+                $type = $this->get_type_from_title($entity['title']);
+            }
+            switch ($type) {
+                case 'passage':
+                    $passage = new TweePassage($entity);
+                    $this->passages[$passage->title] = $passage;
+                    // Set the start passage
+                    if (in_array('start', $passage->tags)) {
+                        $this->current_passage = $passage->title;
+                        $this->history[] = $this->current_passage;
+                        $this->storyline[] = $this->current_passage;
+                    }
+                    break;
+                case 'title':
+                    $this->title = $entity['text'];
+                    break;
+                case 'script':
+                    $this->scripts[] = $entity['text'];
+                    break;
+                case 'stylesheet':
+                    $this->stylesheets[] = $entity['text'];
+                    break;
+                default:
+                    throw new \Exception("Unknown entity type!");
+            }
+        }
+        return true;
+    }
 
-			// Reset the redo history
-			if($this->history_offset != 0) {
-				$this->history = array_slice($this->history, 0, -$this->history_offset);
-				$this->history_offset = 0;
-			}
-			return $this->get_current_passage();
-		}
-		$this->log->addWarning("Trying to follow a dead link.");
-	}
+    /**
+     * Get the type of entity based on its tags.
+     *
+     * @param Array $tags The tags of the entity
+     *
+     * @return String|Bool The type of entity or FALSE
+     *
+     * @author Julio Foulquie <jfoulquie@gmail.com>
+     */
+    private function get_type_from_tags($tags)
+    {
+        $tag_types = array('stylesheet', 'script');
 
-	/**
-	 * 				☣☣☣☣☣☣☣☣☣☣☣☣
-	 * 				Internal functions
-	 * 				☣☣☣☣☣☣☣☣☣☣☣☣
-	 */
+        foreach ($tag_types as $tag) {
+            if (in_array($tag, $tags)) {
+                return $tag;
+            }
+        }
 
-	/**
-	 * Process the JSON exported from Twee and create the story object
-	 *
-	 * @param String $json The string containig the JSON object
-	 *
-	 * @return void
-	 *
-	 * @author Julio Foulquie <jfoulquie@gmail.com>
-	 */
-	private function process_json($json) {
+        return false;
+    }
 
-		$twee_array = json_decode($json, TRUE);
+    /**
+     * Get the type of entity based on its title.
+     *
+     * @param String $title The title of the entity
+     *
+     * @return String The type of entity
+     *
+     * @author Julio Foulquie <jfoulquie@gmail.com>
+     */
+    private function get_type_from_title($title)
+    {
+        $title_types = array(
+            'stylesheet'    => 'UserStylesheet',
+            'script'        => 'UserScript',
+            'title'         => 'StoryTitle',
+        );
 
-		if(!isset($twee_array['data']) OR empty($twee_array['data'])) {
-			$this->log->addWarning("The provided JSON did not decode or is empty.");
-			$this->log->addWarning($json);
-			return FALSE;
-		}
+        foreach ($title_types as $type => $name) {
+            if ($title == $name) {
+                return $type;
+            }
+        }
 
-		foreach($twee_array['data'] AS $entity) {
-			$type = $this->get_type_from_tags($entity['tags']);
-			if(empty($type)){
-				$type = $this->get_type_from_title($entity['title']);
-			}
-			switch($type) {
-				case 'passage':
-					try {
-						$passage = new TweePassage($entity, $this->log);
-						$this->passages[$passage->title] = $passage;
-                        // Set the start passage
-                        if(in_array('start', $passage->tags)) {
-							$this->current_passage = $passage->title;
-							$this->history[] = $this->current_passage;
-							$this->storyline[] = $this->current_passage;
-						}
-					} catch(Exception $e) {
-						$this->log->addWarning($e->getMessage());
-					}
-					break;
-				case 'title':
-					$this->title = $entity['text'];
-					break;
-				case 'script':
-					$this->scripts[] = $entity['text'];
-					break;
-				case 'stylesheet':
-					$this->stylesheets[] = $entity['text'];
-					break;
-				default:
-					$this->log->addWarning("Unknown entity type!");
-					$this->log->addWarning(print_r($entity, TRUE));
-					break;
-			}
-		}
-		return TRUE;
-	}
-
-	/**
-	 * Get the type of entity based on its tags.
-	 *
-	 * @param Array $tags The tags of the entity
-	 *
-	 * @return String|Bool The type of entity or FALSE
-	 *
-	 * @author Julio Foulquie <jfoulquie@gmail.com>
-	 */
-	private function get_type_from_tags($tags) {
-		$tag_types = array('stylesheet', 'script');
-
-		foreach($tag_types as $tag) {
-			if(in_array($tag, $tags)) {
-				return $tag;
-			}
-		}
-
-		return FALSE;
-	}
-
-	/**
-	 * Get the type of entity based on its title.
-	 *
-	 * @param String $title The title of the entity
-	 *
-	 * @return String The type of entity
-	 *
-	 * @author Julio Foulquie <jfoulquie@gmail.com>
-	 */
-	private function get_type_from_title($title) {
-		$title_types = array(
-			'stylesheet'	=> 'UserStylesheet',
-			'script'		=> 'UserScript',
-			'title'			=> 'StoryTitle',
-		);
-
-		foreach($title_types AS $type => $name) {
-			if($title == $name) {
-				return $type;
-			}
-		}
-		// Let's assume if the title isn't on the list, the entity is a passage
-		return 'passage';
-	}
-
+        // Let's assume if the title isn't on the list, the entity is a passage
+        return 'passage';
+    }
 }
